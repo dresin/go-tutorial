9a86fef52aa51f577dc99d37c1ed3fe88d2a5105
dresin <dusan.s.resin@gmail.com>
2023-05-15 17:41:50 +0200

    update printing

--- 001_printing/001_printing.go
+++ 001_printing/001_printing.go
@@ -25,4 +25,25 @@ func main() {
 
 	//Printf example with tab before the number and with newline
 	fmt.Printf("Printf\t%v\n", 3)
+
+	// Format verbs
+	fmt.Printf("\nPrintf function uses the following verbs for formatting:\n")
+	fmt.Printf("\nGeneral:\n\n")
+	fmt.Println("%v the value in a default format")
+	fmt.Println("%v+ when printing structs adds field names")
+	fmt.Println("%#v a Go-syntax representation of the value")
+	fmt.Println("%T a Go-syntax representation of the type")
+	fmt.Println("%% literal percent sign")
+	fmt.Printf("\nBoolean:\n\n")
+	fmt.Println("%t the word true or false")
+	fmt.Printf("\nInteger:\n\n")
+	fmt.Println("%b binary representation - base 2")
+	fmt.Println("%c the character represented by the coresponding Unicode code point")
+	fmt.Println("%d decimal representation - base 10")
+	fmt.Println("%o octal representation - base 8")
+	fmt.Println("%O octal representation with prefix 0o")
+	fmt.Println("%q a single-quoted character literal safely escaped with Go syntax")
+	fmt.Println("%x hexadecimal representation with lower-case letters for a-f")
+	fmt.Println("%X hexadecimal representation with upper-case letters for A-F")
+	fmt.Println("%U Unicode format: U+1234; same as U+%04X")
 }
