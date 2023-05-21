683e48314c770f547f66a663247916558e31368b
dresin <dusan.s.resin@gmail.com>
2023-05-22 01:14:41 +0200

    add variables

---
+++ 005_variables/005_variables.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The variables are declared with the var statement.")
+	var x = 1
+	fmt.Printf("Variable x is declared at the function level with the initial value of %v.", x)
+}
