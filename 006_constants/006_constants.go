829216c6d390d606e2ffa63b5777571fe1578056
dresin <dusan.s.resin@gmail.com>
2023-05-25 14:28:09 +0200

    update constants

--- 006_constants/006_constants.go
+++ 006_constants/006_constants.go
@@ -4,10 +4,17 @@ import "fmt"
 
 const x = 1
 
+const (
+	a = "a1"
+	b = "b2"
+)
+
 func main() {
 	fmt.Println("Constants are declared using the const keyword.")
 	fmt.Println("Constant x is defined at the package level and initialized with the value of 1.")
 	const y = 2.2
 	fmt.Println("Constant y is defined at the function level and initialized with the value of 2.2.")
 	fmt.Println("Constants cannot be declared using the := syntax.")
+	fmt.Println("Constants can be declared together using the const() syntax.")
+	fmt.Printf("Constants a and b have the values of %v and %v.", a, b)
 }
