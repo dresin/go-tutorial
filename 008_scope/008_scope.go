b4d86aabb650f0ce69c6c6f6c6289f670a026122
dresin <dusan.s.resin@gmail.com>
2023-05-29 20:45:10 +0200

    add scope

---
+++ 008_scope/008_scope.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("A declaration binds a non-blank identifier to a specific scope.")
+	fmt.Println("No identifier may be declared more than once in the same block.")
+	fmt.Println("No identifier may be declared in both the package and file block.")
+}
