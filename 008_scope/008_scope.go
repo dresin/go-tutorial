a454e382448262337c6dd2ca15e56a4339114b4e
dresin <dusan.s.resin@gmail.com>
2023-05-30 23:46:31 +0200

    add blank and init identifiers

--- 008_scope/008_scope.go
+++ 008_scope/008_scope.go
@@ -6,4 +6,7 @@ func main() {
 	fmt.Println("A declaration binds a non-blank identifier to a specific scope.")
 	fmt.Println("No identifier may be declared more than once in the same block.")
 	fmt.Println("No identifier may be declared in both the package and file block.")
+	fmt.Println("The blank identifier does not introduce a binding.")
+	fmt.Println("The init identifier may only be used for init function declarations in the package block.")
+	fmt.Println("The init identifier does not introduce a new binding.")
 }
