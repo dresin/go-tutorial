59ab57039b633f40cd19b7a797c78d16919ff1a8
dresin <dusan.s.resin@gmail.com>
2023-07-07 15:36:42 +0200

    add strings

---
+++ 009_strings/009_strings.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("There are two forms of string literals:")
+	fmt.Printf("\t1. Raw string literals\n")
+	fmt.Printf("\t2. Interpreted string literals\n")
+}
