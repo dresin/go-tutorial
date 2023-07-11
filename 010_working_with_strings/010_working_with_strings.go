363eb6ca2dc51f7b71de430254100dd37bfd1553
dresin <dusan.s.resin@gmail.com>
2023-07-11 15:12:58 +0200

    add function Contains from strings package

--- 010_working_with_strings/010_working_with_strings.go
+++ 010_working_with_strings/010_working_with_strings.go
@@ -1,10 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Working with strings")
 	fmt.Printf("\nThe length of a string can be determined using the len() function:")
 	greeting := "Hello!"
 	fmt.Printf("\n\tThe length of string `%s` is %d.", greeting, len(greeting))
+	fmt.Printf("\n\nstrings package")
+	fmt.Printf("\n\nFunction Contains determins if the string contains specific substring:")
+	he := "He"
+	heInGreeting := strings.Contains(greeting, he)
+	fmt.Printf("\n\tDoes string `%s` contain `%s`? %t", greeting, he, heInGreeting)
+	the := "The"
+	theInGreeting := strings.Contains(greeting, the)
+	fmt.Printf("\n\tDoes string `%s` contain `%s`? %t", greeting, the, theInGreeting)
 }
