0f72601f40c1c1efda3766a80476af6ab02cedeb
dresin <dusan.s.resin@gmail.com>
2023-08-28 07:28:26 +0200

    add numbers

---
+++ 011_numbers/011_numbers.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("There are three numeric types in Go:")
+	fmt.Printf("\tinteger\n")
+	fmt.Printf("\tfloating-point\n")
+	fmt.Printf("\tcomplex\n\n")
+	fmt.Println("The integer type has two variations:")
+	fmt.Printf("\tunsigned integer\n")
+	fmt.Printf("\tsigned integer\n\n")
+}
