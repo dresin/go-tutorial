dd5a3cf7667741a387f5831f2a7b5186c3447f05
dresin <dusan.s.resin@gmail.com>
2023-09-10 16:52:32 +0200

    add arithmetic operators

--- 012_working_with_numbers/012_working_with_numbers.go
+++ 012_working_with_numbers/012_working_with_numbers.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Working with numbers")
+	fmt.Printf("Working with numbers\n\n")
+	fmt.Println("Arithmetic operators (and the types they apply to):")
+	fmt.Printf("\t+\taddition\t(integer, float, complex)\n")
+	fmt.Printf("\t-\tsubtraction\t(integer, float, complex)\n")
+	fmt.Printf("\t*\tmultiplication\t(integer, float, complex)\n")
+	fmt.Printf("\t/\tdivision\t(integer, float, complex)\n")
+	fmt.Printf("\t%%\tmodulo\t\t(integer)\n")
 }
