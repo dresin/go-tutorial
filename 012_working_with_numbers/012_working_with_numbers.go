237684ecb6d32fea3b5b1c1f2a6fe09177eaf6b0
dresin <dusan.s.resin@gmail.com>
2023-09-11 15:48:38 +0200

    add integer overflow

--- 012_working_with_numbers/012_working_with_numbers.go
+++ 012_working_with_numbers/012_working_with_numbers.go
@@ -9,5 +9,8 @@ func main() {
 	fmt.Printf("\t-\tsubtraction\t(integer, float, complex)\n")
 	fmt.Printf("\t*\tmultiplication\t(integer, float, complex)\n")
 	fmt.Printf("\t/\tdivision\t(integer, float, complex)\n")
-	fmt.Printf("\t%%\tmodulo\t\t(integer)\n")
+	fmt.Printf("\t%%\tmodulo\t\t(integer)\n\n")
+	fmt.Printf("Integer overflow\n")
+	fmt.Printf("\tWorking with unsigned integers using operators +, - and * may cause an overflow.\n")
+
 }
