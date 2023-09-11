b74ec220e580a45efc584cf02953e57445379800
dresin <dusan.s.resin@gmail.com>
2023-09-11 15:50:46 +0200

    add integer overflow example for plus operator

--- 012_working_with_numbers/012_working_with_numbers.go
+++ 012_working_with_numbers/012_working_with_numbers.go
@@ -12,5 +12,16 @@ func main() {
 	fmt.Printf("\t%%\tmodulo\t\t(integer)\n\n")
 	fmt.Printf("Integer overflow\n")
 	fmt.Printf("\tWorking with unsigned integers using operators +, - and * may cause an overflow.\n")
-
+	fmt.Printf("\tOverflow examples:\n")
+	fmt.Printf("\t\tAdding 1 to the largest possible uint8 number using operator +:\n")
+	fmt.Printf("\t\t\tvar x uint8 = 255\n")
+	var x uint8 = 255
+	fmt.Printf("\t\t\tfmt.Printf(\"x = %%v\", x)\n")
+	fmt.Printf("\t\t\t// the previous line will output: ")
+	fmt.Printf("x = %v\n", x)
+	fmt.Printf("\t\t\tfmt.Printf(\"x + 1 = %%v\", x + 1)\n")
+	fmt.Printf("\t\t\t// the previous line will output: ")
+	fmt.Printf("x + 1 = %v\n", x+1)
+	fmt.Printf("\t\t\t// 255 + 1 will equal to 0, and not to 256, because of the overflow\n")
+	fmt.Printf("\t\t\t// the overflow happens because uint8 type has a range from 0 to 255\n")
 }
