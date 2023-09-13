6b63d0971c8eccc83d47068ef3ee9ff6bddf8cb8
dresin <dusan.s.resin@gmail.com>
2023-09-13 15:05:37 +0200

    add integer overflow example for star operator

--- 012_working_with_numbers/012_working_with_numbers.go
+++ 012_working_with_numbers/012_working_with_numbers.go
@@ -35,4 +35,15 @@ func main() {
 	fmt.Printf("y - 1 = %v\n", y-1)
 	fmt.Printf("\t\t\t// 0 - 1 will equal to 255, and not to -1, because of the overflow\n")
 	fmt.Printf("\t\t\t// the overflow happens because uint8 type has a range from 0 to 255\n")
+	fmt.Printf("\t\tMultiplying a uint8 number using operator *:\n")
+	fmt.Printf("\t\t\tvar z uint8 = 100\n")
+	var z uint8 = 100
+	fmt.Printf("\t\t\tfmt.Printf(\"z = %%v\", z)\n")
+	fmt.Printf("\t\t\t// the previous line will output: ")
+	fmt.Printf("z = %v\n", z)
+	fmt.Printf("\t\t\tfmt.Printf(\"z * 3 = %%v\", z * 3)\n")
+	fmt.Printf("\t\t\t// the previous line will output: ")
+	fmt.Printf("z * 3 = %v\n", z*3)
+	fmt.Printf("\t\t\t// 100 * 3 will equal to 44, and not to 300, because of the overflow\n")
+	fmt.Printf("\t\t\t// the overflow happens because uint8 type has a range from 0 to 255\n")
 }
