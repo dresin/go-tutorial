7766914ddc81f8a293723d06c457605c884e9125
dresin <dusan.s.resin@gmail.com>
2023-09-22 18:12:38 +0200

    add working with booleans

---
+++ 014_working_with_booleans/014_working_with_booleans.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Working with booleans\n\n")
+	fmt.Println("Logical operators:")
+	fmt.Printf("\t&&\tAND\n")
+	fmt.Printf("\t||\tOR\n")
+	fmt.Printf("\t!\tNOT\n")
+}
